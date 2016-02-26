9d92c43cd6f5028355258f698d4633b2a4f3be8d
Mark Goddard <mgoddard@cray.com>
2016-02-26 19:16:35 +0200

    Add functional tests to Bareon
    Change-Id: Idc60cca46d6d0feea3eb1cf7d23a97811c057d48
    Implements: blueprint bareon-functional-testing

---
+++ bareon/tests_functional/image_build/centos_minimal_env.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# Copyright 2016 Cray Inc.  All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# A default build environment used to build CentOS-minimal image
+
+export DIB_SRC=git@github.com:openstack/diskimage-builder.git
+export DIB_BRANCH=master
+
+export DIB_UTILS_SRC=git@github.com:openstack/dib-utils.git
+export DIB_UTILS_BRANCH=master
+
+export FUEL_KEY=https://raw.githubusercontent.com/stackforge/fuel-main/master/bootstrap/ssh/id_rsa
+export BUILD_DIR=/tmp/rft_image_build
