bbe4f67b25cfcc97f150d38c5003a3c224cc46d5
Alexander Gordeev <agordeev@mirantis.com>
2016-04-05 17:06:35 +0300

    Introduce PluggableDeployDriver
    Change-Id: I1bb6a5e904741a8689d08e1939a3e098864a5d03

---
+++ bareon/drivers/deploy/pluggable.py
@@ -0,0 +1,37 @@
+#
+# Copyright 2016 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from bareon.drivers.deploy import base
+from bareon.drivers.deploy import mixins
+from bareon.openstack.common import log as logging
+
+import stevedore.named
+
+LOG = logging.getLogger(__name__)
+
+_DO_ACTIONS_NS = 'bareon.do_actions'
+
+
+class PluggableDeployDriver(base.SimpleDeployDriver, mixins.MountableMixin):
+
+    def __init__(self, data_driver):
+        super(PluggableDeployDriver, self).__init__(data_driver)
+        self.ext_mgr = stevedore.named.NamedExtensionManager(
+            _DO_ACTIONS_NS, names=self.driver.flow, name_order=True)
+
+    def do_actions(self):
+        for action in self.ext_mgr:
+            action.validate()
+            action.execute()
