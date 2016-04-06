0e86a2f0a5fe29ec08d05f5a9d4d317aae626c66
Dmitry Guryanov <dguryanov@mirantis.com>
2016-04-06 21:01:16 +0300

    change configdrive format to ConfigDrive version 2
    We put configdrive with is9660 filesystem to a
    partition on a hard disk. New hard disks may have
    4K sectors, but blocksize of iso9660 fs is 2K so
    it will not work.
    
    To fix this bug we should use another filesystem (ext2)
    and another config drive format (files, directory
    structure), because NoCloud format, which is currently
    used support only vfat and iso9660 filesystems.
    
    Conflicts:
    
    	bareon/drivers/deploy/nailgun.py
    	fuel_agent/tests/test_manager.py
    
    Change-Id: Ia0f244f19bab3dfaceef8a092ad03667675a5557
    Closes-Bug: #1544818

--- bareon/tests/test_do_configdrive.py
+++ bareon/tests/test_do_configdrive.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import six
 import unittest2
 
@@ -40,16 +41,30 @@ class TestConfigDriveAction(unittest2.TestCase):
             profile='pro_fi-le')
         self.drv.configdrive_scheme.template_data = mock.Mock()
         self.drv.image_scheme = objects.ImageScheme()
+        self.drv.partition_scheme.configdrive_device.return_value = '/dev/sda7'
 
+    def test_do_configdrive(self):
+        with mock.patch.multiple(self.action,
+                                 _prepare_configdrive_files=mock.DEFAULT,
+                                 _make_configdrive_image=mock.DEFAULT,
+                                 _add_configdrive_image=mock.DEFAULT) as mocks:
+            mocks['_prepare_configdrive_files'].return_value = 'x'
+            self.action.execute()
+            mocks['_prepare_configdrive_files'].assert_called_once_with()
+            mocks['_make_configdrive_image'].assert_called_once_with('x')
+            mocks['_add_configdrive_image'].assert_called_once_with()
+
+    @mock.patch.object(configdrive, 'tempfile', autospec=True)
     @mock.patch.object(configdrive, 'os', autospec=True)
     @mock.patch.object(configdrive, 'utils', autospec=True)
-    def test_do_configdrive(self, mock_utils, mock_os):
-        self.drv.partition_scheme.configdrive_device.return_value = '/dev/sda7'
-        mock_os.path.getsize.return_value = 123
-        mock_os.path.join = lambda x, y: '%s/%s' % (x, y)
-        mock_utils.calculate_md5.return_value = 'fakemd5'
-        self.assertEqual(0, len(self.drv.image_scheme.images))
-        self.action.execute()
+    def test_prepare_configdrive_files(self, mock_utils, mock_os, mock_temp):
+        mock_os.path.join = os.path.join
+        mock_temp.mkdtemp.return_value = '/tmp/qwe'
+        ret = self.action._prepare_configdrive_files()
+        self.assertEqual(ret, ['/tmp/qwe/openstack'])
+        mock_temp.mkdtemp.assert_called_once_with(dir=CONF.tmp_path)
+        mock_os.makedirs.assert_called_once_with('/tmp/qwe/openstack/latest')
+
         mock_u_ras_expected_calls = [
             mock.call(CONF.nc_template_path,
                       ['cloud_config_pro_fi-le.jinja2',
@@ -64,41 +79,83 @@ class TestConfigDriveAction(unittest2.TestCase):
                        'boothook.jinja2'],
                       mock.ANY, '%s/%s' % (CONF.tmp_path, 'boothook.txt')),
             mock.call(CONF.nc_template_path,
-                      ['meta_data_pro_fi-le.jinja2',
-                       'meta_data_pro.jinja2',
-                       'meta_data_pro_fi.jinja2',
-                       'meta_data.jinja2'],
-                      mock.ANY, '%s/%s' % (CONF.tmp_path, 'meta-data'))]
+                      ['meta_data_json_pro_fi-le.jinja2',
+                       'meta_data_json_pro.jinja2',
+                       'meta_data_json_pro_fi.jinja2',
+                       'meta_data_json.jinja2'],
+                      mock.ANY, '/tmp/qwe/openstack/latest/meta_data.json')]
         self.assertEqual(mock_u_ras_expected_calls,
                          mock_utils.render_and_save.call_args_list)
 
-        mock_u_e_expected_calls = [
-            mock.call('write-mime-multipart',
-                      '--output=%s' % ('%s/%s' % (CONF.tmp_path, 'user-data')),
-                      '%s:text/cloud-boothook' % ('%s/%s' % (CONF.tmp_path,
-                                                             'boothook.txt')),
-                      '%s:text/cloud-config' % ('%s/%s' % (CONF.tmp_path,
-                                                           'cloud_config.txt'))
-                      ),
-            mock.call('genisoimage', '-output', CONF.config_drive_path,
-                      '-volid', 'cidata', '-joliet', '-rock',
-                      '%s/%s' % (CONF.tmp_path, 'user-data'),
-                      '%s/%s' % (CONF.tmp_path, 'meta-data'))]
-        self.assertEqual(mock_u_e_expected_calls,
-                         mock_utils.execute.call_args_list)
+        mock_utils.execute.assert_called_once_with(
+            'write-mime-multipart',
+            '--output=/tmp/qwe/openstack/latest/user_data',
+            '%s/%s:text/cloud-boothook' % (CONF.tmp_path, 'boothook.txt'),
+            '%s/%s:text/cloud-config' % (CONF.tmp_path, 'cloud_config.txt'))
+
+    @mock.patch.object(configdrive, 'tempfile', autospec=True)
+    @mock.patch.object(configdrive, 'shutil', autospec=True)
+    @mock.patch.object(configdrive, 'fu', autospec=True)
+    @mock.patch.object(configdrive, 'os', autospec=True)
+    @mock.patch.object(configdrive, 'utils', autospec=True)
+    def test_make_configdrive_image(self, mock_utils, mock_os, mock_fu,
+                                    mock_shutil, mock_temp):
+        mock_utils.execute.side_effect = [(' 795648', ''), None]
+        mock_os.path.isdir.side_effect = [True, False]
+        mock_os.path.join = os.path.join
+        mock_os.path.basename = os.path.basename
+
+        mock_temp.mkdtemp.return_value = '/tmp/mount_point'
+
+        self.action._make_configdrive_image(['/tmp/openstack',
+                                             '/tmp/somefile'])
+
+        mock_u_e_calls = [
+            mock.call('blockdev', '--getsize64', '/dev/sda7'),
+            mock.call('truncate', '--size=795648', CONF.config_drive_path)]
+
+        self.assertEqual(mock_u_e_calls, mock_utils.execute.call_args_list,
+                         str(mock_utils.execute.call_args_list))
+
+        mock_fu.make_fs.assert_called_with(fs_type='ext2',
+                                           fs_options=' -b 4096 -F ',
+                                           fs_label='config-2',
+                                           dev=CONF.config_drive_path)
+        mock_fu.mount_fs.assert_called_with('ext2',
+                                            CONF.config_drive_path,
+                                            '/tmp/mount_point')
+        mock_fu.umount_fs.assert_called_with('/tmp/mount_point')
+        mock_os.rmdir.assert_called_with('/tmp/mount_point')
+        mock_shutil.copy2.assert_called_with('/tmp/somefile',
+                                             '/tmp/mount_point')
+        mock_shutil.copytree.assert_called_with('/tmp/openstack',
+                                                '/tmp/mount_point/openstack')
+
+    @mock.patch.object(configdrive, 'fu', autospec=True)
+    @mock.patch.object(configdrive, 'os', autospec=True)
+    @mock.patch.object(configdrive, 'utils', autospec=True)
+    def test_add_configdrive_image(self, mock_utils, mock_os, mock_fu):
+        mock_fu.get_fs_type.return_value = 'ext999'
+        mock_utils.calculate_md5.return_value = 'fakemd5'
+        mock_os.path.getsize.return_value = 123
+
+        self.action._add_configdrive_image()
+
         self.assertEqual(1, len(self.drv.image_scheme.images))
-        cf_drv_img = self.drv.image_scheme.images[-1]
+        cf_drv_img = self.drv.image_scheme.images[0]
         self.assertEqual('file://%s' % CONF.config_drive_path, cf_drv_img.uri)
-        self.assertEqual('/dev/sda7',
-                         self.drv.partition_scheme.configdrive_device())
-        self.assertEqual('iso9660', cf_drv_img.format)
+        self.assertEqual('/dev/sda7', cf_drv_img.target_device)
+        self.assertEqual('ext999', cf_drv_img.format)
         self.assertEqual('raw', cf_drv_img.container)
         self.assertEqual('fakemd5', cf_drv_img.md5)
         self.assertEqual(123, cf_drv_img.size)
 
     @mock.patch.object(configdrive, 'os', autospec=True)
     @mock.patch.object(configdrive, 'utils', autospec=True)
-    def test_do_configdrive_no_configdrive_device(self, mock_utils, mock_os):
+    def test_add_configdrive_image_no_configdrive_device(self, mock_utils,
+                                                         mock_os):
         self.drv.partition_scheme.configdrive_device.return_value = None
+        mock_utils.calculate_md5.return_value = 'fakemd5'
+        mock_os.path.getsize.return_value = 123
         self.assertRaises(errors.WrongPartitionSchemeError,
-                          self.action.execute)
+                          self.action._add_configdrive_image)
