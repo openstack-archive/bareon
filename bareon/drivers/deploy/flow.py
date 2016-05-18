6567a6ea251f53486e1d6011b6413ae526e44cae
Alexander Gordeev <agordeev@mirantis.com>
2016-05-18 20:37:12 +0300

    Introduce ability to execute custom flow
    Flow deploy driver is now capable of executing customly defined flow
    of actions.
    When custom flow is chooen, then the flow itself must be provided
    within input data as a list of actions under 'custom_flow' key.
    This is the only beggining and only 2 flows are made available:
    custom and nailgun.
    
    Partially implements blueprint: pluggable-do-actions
    
    Change-Id: I3cf3fcabb47bdc7d1aadb97614310ee3184ed77c

--- bareon/drivers/deploy/flow.py
+++ bareon/drivers/deploy/flow.py
@@ -15,21 +15,42 @@
 
 from bareon.drivers.deploy import base
 from bareon.drivers.deploy import mixins
+from bareon import errors
 from bareon.openstack.common import log as logging
 
 import stevedore.named
 
 LOG = logging.getLogger(__name__)
+FLOWS = {
+    'nailgun': [
+        'do_partitioning',
+        'do_configdrive',
+        'do_copyimage',
+        'do_bootloader'
+    ],
+    'custom': [],
+}
 
 
 class Flow(base.SimpleDeployDriver, mixins.MountableMixin):
 
     def __init__(self, data_driver):
         super(Flow, self).__init__(data_driver)
+        requested_flow = self.driver.flow
+        if requested_flow in FLOWS:
+            if requested_flow == 'custom':
+                flow = self.driver.data.get('custom_flow', [])
+                if not flow:
+                    raise errors.EmptyCustomFlow(
+                        'Requested custom flow was empty')
+            else:
+                flow = FLOWS[requested_flow]
+        else:
+            raise errors.NonexistingFlow(
+                "Requested flow doesn't exist: %s" % requested_flow)
         self.ext_mgr = stevedore.named.NamedExtensionManager(
-            'bareon.actions', names=self.driver.flow,
-            invoke_on_load=True, invoke_args=(self.driver, ),
-            name_order=True)
+            'bareon.actions', names=flow, invoke_on_load=True,
+            invoke_args=(self.driver, ), name_order=True)
 
     def execute_flow(self):
         for action in self.ext_mgr:
