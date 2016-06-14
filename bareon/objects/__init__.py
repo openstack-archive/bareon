stackforge/fuel-agent

Added User object
User object represents a user account on the target
Operating System.

Change-Id: I70dacf14d4ddf817742008ec7e5e40d4d8c18cca
Blueprint: fuel-nonroot-openstack-nodes

fuel_agent/objects/__init__.py
@@ -33,6 +33,7 @@ from fuel_agent.objects.partition.vg import VolumeGroup
 from fuel_agent.objects.repo import DEBRepo
 from fuel_agent.objects.repo import Repo
 from fuel_agent.objects.repo import RepoProxies
+from fuel_agent.objects.user import User
 
 
 PV = PhysicalVolume
@@ -69,5 +70,6 @@ __all__ = [
     'Repo',
     'DEBRepo',
     'Loop',
-    'RepoProxies'
+    'RepoProxies',
+    'User'
 ]

openstack-archive/bareon

Conflicts:
	fuel_agent/objects/__init__.py

bareon/objects/__init__.py
@@ -33,6 +33,7 @@ from bareon.objects.partition.vg import VolumeGroup
 from bareon.objects.repo import DEBRepo
 from bareon.objects.repo import Repo
 from bareon.objects.repo import RepoProxies
+from bareon.objects.users import User
 
 
 PV = PhysicalVolume
@@ -69,5 +70,6 @@ __all__ = [
     'Repo',
     'DEBRepo',
     'Loop',
-    'RepoProxies'
+    'RepoProxies',
+    'User',
 ]